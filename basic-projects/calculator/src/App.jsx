b1511574dc738668f68917e65c6cf1d0a3937d90
poojaa09 <poojarawat0329@gmail.com>
2025-05-16 01:03:54 +0530

    basic calc done

---
+++ basic-projects/calculator/src/App.jsx
@@ -0,0 +1,18 @@
+import Display from './components/Display'
+import Button from './components/Button'
+import styles from './App.module.css'
+
+function App() {
+ 
+
+  return (<center>
+    <div className={styles.calc}>
+      
+     
+      <Display/>
+      <Button/>
+    </div></center>
+  )
+}
+
+export default App
